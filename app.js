a5e46a750c28801997949a225d637cfd76472d76
IsMaa <ismael_ideme91@hotmail.com>
2023-01-13 09:11:29 -0300

    agrego rama formularios incompleta

--- app.js
+++ app.js
@@ -8,4 +8,6 @@ const port = 3030
 app.use (express.static(path.resolve(__dirname, "public")))
 app.listen(port, () => `Servidor levantado en puerto ${port}`)
 
-app.get("/", (req,res) =>res.sendFile(path.resolve(__dirname,"views","home.html")))
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "views", "home.html")))
+app.get("/login.html", (req, res) => res.sendFile(path.resolve(__dirname, "views", "login.html")))
+app.get("/register.html", (req,res) =>res.sendFile(path.resolve(__dirname,"views","register.html"))) 
